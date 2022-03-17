3ac9f4de13e476e53ff821321581930ebe1c79a6
Mercedes AC <breakh_070795@hotmail.com>
2022-03-17 14:56:28 -0600

    Primera parte de calculo de imc. No funcional

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-const app = Vue.createApp({
-    template: `
-        
+const Titulo = {
+    template:`
+        <h1>Calculadora de IMC y Grasa Corporal.</h1>
     `
-})
+}
+
+const app = Vue.createApp({
+    components:{
+        Titulo
+    },
+    template: `
+        <Titulo/>
+        <h3>Para calcular el Indice de Masa Corporal, debes ingresar tu peso y estatura.</h3>
+        <div>
+            <p>Ingresa tu peso</p>
+            <input type="number"><br>
+            <p>Ingresa tu estatura</p>
+            <input type="number"> <br> <br>
+            <button @click="calcularIMC">Calcular</button> {{ imc }}
+        </div>
+    ` /* ,
+    data(){
+        return {
+            imc,
+            gc
+        }
+    } 
+    ,
+    methods:{
+         calcularIMC(peso, estatura){
+            imc = peso / (estatura * estatura);
+            return imc;
+        },
+        obtenerGC(edad,  sexo){
+            if("hombre".equals(sexo)){
+                gc=1.2*imc+0.23*edad-10.8*1-5.4;
+			return gc;
+            }else{
+                gc=1.2*imc+0.23*edad-10.8*0-5.4;
+			return gc;
+            }
+        }
+    }  */
+}).mount('#app');
